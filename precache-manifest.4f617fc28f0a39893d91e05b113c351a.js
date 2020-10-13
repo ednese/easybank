14f0410a746bb77fa700c8ef255172355b6c7bc1
ednese <evancours94@gmail.com>
2020-10-13 05:04:53 +0200

    Updates

---
+++ precache-manifest.4f617fc28f0a39893d91e05b113c351a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "134943df157ae2d5f04b645feda0ee83",
+    "url": "/easybank/index.html"
+  },
+  {
+    "revision": "1a957b19c4bb61a91097",
+    "url": "/easybank/static/css/main.e8427d63.chunk.css"
+  },
+  {
+    "revision": "909cddb5dbd651fee59b",
+    "url": "/easybank/static/js/2.4a126b34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/easybank/static/js/2.4a126b34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a957b19c4bb61a91097",
+    "url": "/easybank/static/js/main.4afec9c6.chunk.js"
+  },
+  {
+    "revision": "b17b429704d25d4675e3",
+    "url": "/easybank/static/js/runtime-main.e87b0b0f.js"
+  },
+  {
+    "revision": "9cea2f98108b16ed09dec91dc0e5a4d4",
+    "url": "/easybank/static/media/bg-intro-desktop.9cea2f98.svg"
+  },
+  {
+    "revision": "c23dd1d0b7e4b4a1637d50cbb9fff810",
+    "url": "/easybank/static/media/bg-intro-mobile.c23dd1d0.svg"
+  },
+  {
+    "revision": "db3d89069f8292defec0f180493a8817",
+    "url": "/easybank/static/media/icon-api.db3d8906.svg"
+  },
+  {
+    "revision": "d4cdc9d6b3c930ddaff8dc79acc005a1",
+    "url": "/easybank/static/media/icon-budgeting.d4cdc9d6.svg"
+  },
+  {
+    "revision": "1a81861b7aaa8c30169d65faf9027218",
+    "url": "/easybank/static/media/icon-facebook.1a81861b.svg"
+  },
+  {
+    "revision": "e0814599d1e638a04578e79a652801c0",
+    "url": "/easybank/static/media/icon-instagram.e0814599.svg"
+  },
+  {
+    "revision": "2fc9c776f2bb1b98e1d23bc813261bd3",
+    "url": "/easybank/static/media/icon-onboarding.2fc9c776.svg"
+  },
+  {
+    "revision": "7a9111dbfa3b4b2ea0d088435bfe1d76",
+    "url": "/easybank/static/media/icon-online.7a9111db.svg"
+  },
+  {
+    "revision": "ef15af23a9c6edf1014f6f44b31c8b78",
+    "url": "/easybank/static/media/icon-pinterest.ef15af23.svg"
+  },
+  {
+    "revision": "064c31714c77b6580c3e4e8176c435fb",
+    "url": "/easybank/static/media/icon-twitter.064c3171.svg"
+  },
+  {
+    "revision": "8e2f566fcefd23c8aacdaa5a83cdaff2",
+    "url": "/easybank/static/media/icon-youtube.8e2f566f.svg"
+  },
+  {
+    "revision": "1510e4a211f7a044236decc6d9b4afe9",
+    "url": "/easybank/static/media/image-confetti.1510e4a2.jpg"
+  },
+  {
+    "revision": "0f583e2b521e1590dd7ab8366c478227",
+    "url": "/easybank/static/media/image-currency.0f583e2b.jpg"
+  },
+  {
+    "revision": "edc63e85927303b6c172617a5d1742b6",
+    "url": "/easybank/static/media/image-mockups.edc63e85.png"
+  },
+  {
+    "revision": "c619f6cb0554ff2c465d2bb6268cb493",
+    "url": "/easybank/static/media/image-plane.c619f6cb.jpg"
+  },
+  {
+    "revision": "b561f8d50067b0ce1c2d4e8b54671922",
+    "url": "/easybank/static/media/image-restaurant.b561f8d5.jpg"
+  },
+  {
+    "revision": "9fa4c7289672ac2e644d48c9ff3bcbbd",
+    "url": "/easybank/static/media/logo-white.9fa4c728.svg"
+  },
+  {
+    "revision": "fe5c6dcee699e86a25a18112801ec30f",
+    "url": "/easybank/static/media/logo.fe5c6dce.svg"
+  }
+]);
